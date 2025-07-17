eeb550f4630b201dd2f0efbe99dd3a5f2490532f
cataML <catelilungo@gmail.com>
2025-07-17 16:10:03 +0300

    Added contact us page

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
 # Create your views here.
 def index(request):
@@ -7,3 +8,12 @@ def index(request):
 
 def about(request):
     return render(request, 'home/about.html')
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            return render(request, 'home/thank_you.html')
+    else:
+        form = ContactForm()
+    return render(request, 'home/contact.html', {'form':form})
